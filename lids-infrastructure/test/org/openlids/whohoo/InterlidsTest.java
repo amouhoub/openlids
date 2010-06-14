ebcd1d1419bcc9ad3a54b72998e31902ca12c271
non88sense <non88sense@f41b035b-029f-3628-e1ad-d1d8cd72057c>
2010-06-14 14:57:06 +0000

    added test case

---
+++ lids-infrastructure/test/org/openlids/whohoo/InterlidsTest.java
@@ -0,0 +1,52 @@
+package org.openlids.whohoo;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import junit.framework.TestCase;
+
+import org.openlids.interfacing.ServiceAnnotator;
+import org.openlids.parser.ServiceParser;
+import org.openlids.parser.ServiceParserJena;
+
+public class InterlidsTest extends TestCase {
+	public void testInterlids() throws Exception {
+		FileInputStream fin = new FileInputStream("files/ssp-foaf.nt");
+
+		FileInputStream sin = new FileInputStream("files/lids.rq");
+		String q = InterlidsTest.streamToString(sin);
+
+		ServiceParser sp = new ServiceParserJena();
+
+		ServiceAnnotator sas = new ServiceAnnotator(sp.parseServiceDescription(q));
+		
+		Model m = ModelFactory.createDefaultModel();
+		m.read(fin, "http://example.org/", "N3");
+
+		m = sas.annotate(m);
+
+		m.write(System.out,"N-TRIPLE");
+	}
+
+	public static String streamToString(InputStream is) throws IOException {
+		StringBuilder sb = new StringBuilder();
+
+		if (is != null) {
+			String line;
+
+			try {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+				while ((line = reader.readLine()) != null) {
+					sb.append(line).append("\n");
+				}
+			} finally {
+				is.close();
+			}
+		}
+
+		return sb.toString();
+	}
+}
