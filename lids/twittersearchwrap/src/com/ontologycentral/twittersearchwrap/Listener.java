7711fc3b173a990dba7d460fde7f831228bbe070
non88sense <non88sense@f41b035b-029f-3628-e1ad-d1d8cd72057c>
2010-12-01 14:02:22 +0000

    moved searchwrap

---
+++ lids/twittersearchwrap/src/com/ontologycentral/twittersearchwrap/Listener.java
@@ -0,0 +1,50 @@
+package com.ontologycentral.twittersearchwrap;
+
+import java.util.Collections;
+
+import javax.cache.Cache;
+import javax.cache.CacheException;
+import javax.cache.CacheFactory;
+import javax.cache.CacheManager;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.stream.StreamSource;
+
+public class Listener implements ServletContextListener {
+	public static String T = "tranformer";
+	public static String CACHE = "c";
+	
+	public void contextInitialized(ServletContextEvent event) {
+		ServletContext ctx = event.getServletContext();
+
+		//TransformerFactory tf = TransformerFactory.newInstance();
+		
+	    Cache cache = null;
+
+	    try {
+	        CacheFactory cacheFactory = CacheManager.getInstance().getCacheFactory();
+	        cache = cacheFactory.createCache(Collections.emptyMap());
+			ctx.setAttribute(CACHE, cache);
+	    } catch (CacheException e) {
+	    	e.printStackTrace();
+	    }
+        
+		javax.xml.transform.TransformerFactory tf =
+			javax.xml.transform.TransformerFactory.newInstance("org.apache.xalan.processor.TransformerFactoryImpl", this.getClass().getClassLoader() ); 
+		
+		try {
+			Transformer t = tf.newTransformer(new StreamSource(ctx.getRealPath("/WEB-INF/search.xsl")));
+			ctx.setAttribute(T, t);
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+		
+	public void contextDestroyed(ServletContextEvent event) {
+		//ServletContext ctx = event.getServletContext();
+	}
+}
