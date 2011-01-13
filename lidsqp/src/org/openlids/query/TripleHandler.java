27e2a6800235ea14eb7cf328aaf7beeb8df164ff
sebastian.speiser <sebastian.speiser@gmail.com>
2011-01-13 19:07:11 +0000

    delete lidsqp

--- lidsqp/src/org/openlids/query/TripleHandler.java
+++
@@ -1,9 +0,0 @@
-package org.openlids.query;
-
-import org.semanticweb.yars.nx.Node;
-
-public interface TripleHandler {
-
-	public Node[] handle(Node[] nx);
-	
-}
--- lidsqp/src/org/openlids/query/arq/QueryEngineConstants.java
+++
@@ -1,6 +0,0 @@
-package org.openlids.query.arq;
-
-public class QueryEngineConstants {
-	public static int QE_THREADS = 1;
-	public static int QE_HOPS = 0;
-}
